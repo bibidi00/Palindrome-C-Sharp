6914ee0e2280224389db7a4f09d1e9ebd2eb8285
bibidi00 <fab_handt@web.de>
2019-04-10 15:02:34 +0200

    ...

---
+++ Palindrome/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(is_palindrome("John"));      // false
+            Console.WriteLine(is_palindrome("Hannah"));    // true
+            Console.WriteLine(is_palindrome("Kiwik"));     // true
+            Console.ReadKey();
+        }
+
+        public static Boolean is_palindrome(String word)
+        {
+            String reverseword = "";
+            Char[] array = word.ToLower().ToCharArray();
+            Array.Reverse(array);
+            foreach(Char a in array)
+            {
+                reverseword += a.ToString();
+            }
+
+            return word.ToLower().Equals(reverseword);
+        }
+    }
+}
